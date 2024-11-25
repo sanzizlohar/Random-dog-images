5533e68381a9a1490adb591f2dca560bf986fd22
Soumen Lohar <somulohar120@gmail.com>
2024-11-26 02:43:07 +0530

    button add

--- main.js
+++ main.js
@@ -5,17 +5,25 @@ const getFacts = async () => {
     const response = await fetch(url);
     const data = await response.json();
 
+    // Select the container div
+    const container = document.querySelector(".container");
+
+    // Clear any existing content in the container
+    container.innerHTML = "";
+
     let img = document.createElement("img");
 
     img.src = data.message;
-    img.alt = "Ramdon Kutta";
+    img.alt = "Random Kutta";
     img.style.width = "300px";
     img.style.height = "auto";
 
-    document.querySelector(".container").append(img);
+    document.querySelector(".container").appendChild(img);
   } catch (err) {
     console.error("Error to fetch image of dogs", err);
   }
 };
+
+document.querySelector("button").addEventListener("click", getFacts);
 
 getFacts();
